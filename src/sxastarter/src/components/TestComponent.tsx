397ce8ae60ade4e3b01b2ab3a983435a033cae3e
Srikar <srikar1248@gmail.com>
2023-02-20 16:57:33 +0530

    Add video

--- src/sxastarter/src/components/TestComponent.tsx
+++ src/sxastarter/src/components/TestComponent.tsx
@@ -1,17 +1,28 @@
 import { Text, Field, withDatasourceCheck } from '@sitecore-jss/sitecore-jss-nextjs';
 import { ComponentProps } from 'lib/component-props';
+import ReactPlayer from 'react-player';
+import { useEffect, useState } from 'react';
 
 type TestComponentProps = ComponentProps & {
   fields: {
     heading: Field<string>;
+    url: Field<string>;
   };
 };
 
-const TestComponent = (props: TestComponentProps): JSX.Element => (
-  <div>
-    <p>TestComponent Component</p>
-    <Text field={props.fields.heading} />
-  </div>
-);
+const TestComponent = (props: TestComponentProps): JSX.Element => {
+  const [domLoaded, setDomLoaded] = useState(false);
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
+  const url = props.fields.url?.value; //'https://www.youtube.com/watch?v=wWgIAphfn2U';
+  return (
+    <div>
+      <p>TestComponent Component</p>
+      <Text field={props.fields.heading} />
+      {domLoaded ? <ReactPlayer url={url} /> : null}
+    </div>
+  );
+};
 
 export default withDatasourceCheck()<TestComponentProps>(TestComponent);
