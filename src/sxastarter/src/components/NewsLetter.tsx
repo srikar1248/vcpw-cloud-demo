bd9ae8bfef59f8ff6aad81aa9ec6e75e4dc9ba59
Srikar <srikar1248@gmail.com>
2023-02-23 12:26:09 +0530

    Modified newsletter component Added form and client script to handle the form submit

--- src/sxastarter/src/components/NewsLetter.tsx
+++ src/sxastarter/src/components/NewsLetter.tsx
@@ -5,6 +5,7 @@ import {
   withDatasourceCheck,
 } from '@sitecore-jss/sitecore-jss-nextjs';
 import { ComponentProps } from 'lib/component-props';
+import { useState } from 'react';
 type NewsLetterProps = ComponentProps & {
   fields: {
     title: Field<string>;
@@ -13,44 +14,68 @@ type NewsLetterProps = ComponentProps & {
     signUpButton: Field<string>;
   };
 };
-const NewsLetter = (props: NewsLetterProps): JSX.Element => (
-  <div className="container px-5 my-5">
-    <aside className="bg-primary bg-gradient rounded-3 p-4 p-sm-5 mt-5">
-      <div className="d-flex align-items-center justify-content-between flex-column flex-xl-row text-center text-xl-start">
-        <div className="mb-4 mb-xl-0">
-          <JssRichText
-            tag="div"
-            className="fs-3 fw-bold text-white"
-            field={props?.fields?.title as Field<string>}
-          />
-          <JssText
-            tag="div"
-            className="text-white-50"
-            field={props?.fields?.description as Field<string>}
-          />
-        </div>
-        <div className="ms-xl-4">
-          <div className="input-group mb-2">
-            <input
-              className="form-control"
-              type="text"
-              placeholder="Email address..."
-              aria-label="Email address..."
-              aria-describedby="button-newsletter"
+interface FormData {
+  email: string;
+}
+const NewsLetter = (props: NewsLetterProps): JSX.Element => {
+  const [formData, setFormData] = useState<FormData>({ email: '' });
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Send form data to server or API
+    console.log(formData.email);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+  return (
+    <div className="container px-5 my-5">
+      <aside className="bg-primary bg-gradient rounded-3 p-4 p-sm-5 mt-5">
+        <div className="d-flex align-items-center justify-content-between flex-column flex-xl-row text-center text-xl-start">
+          <div className="mb-4 mb-xl-0">
+            <JssRichText
+              tag="div"
+              className="fs-3 fw-bold text-white"
+              field={props?.fields?.title as Field<string>}
             />
-            <button className="btn btn-outline-light" id="button-newsletter" type="button">
-              <JssText field={props?.fields?.signUpButton as Field<string>} />
-            </button>
+            <JssText
+              tag="div"
+              className="text-white-50"
+              field={props?.fields?.description as Field<string>}
+            />
           </div>
-          <JssText
-            tag="div"
-            className="small text-white-50"
-            field={props?.fields?.privacyNote as Field<string>}
-          />
+          <div className="ms-xl-4">
+            <div className="input-group mb-2">
+              <form onSubmit={handleSubmit}>
+                <input
+                  name="email"
+                  className="form-control"
+                  type="email"
+                  id="txtemail"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  placeholder="Email address..."
+                  aria-label="Email address..."
+                  aria-describedby="button-newsletter"
+                  required
+                />
+                <button className="btn btn-outline-light" id="button-newsletter" type="submit">
+                  <JssText field={props?.fields?.signUpButton as Field<string>} />
+                </button>
+              </form>
+            </div>
+            <JssText
+              tag="div"
+              className="small text-white-50"
+              field={props?.fields?.privacyNote as Field<string>}
+            />
+          </div>
         </div>
-      </div>
-    </aside>
-  </div>
-);
+      </aside>
+    </div>
+  );
+};
 
 export default withDatasourceCheck()<NewsLetterProps>(NewsLetter);
